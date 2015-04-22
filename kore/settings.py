4916d281087876158d5238faac20aea348b2f55a
Juha Yrjölä <juha.yrjola@iki.fi>
2015-04-22 21:30:53 +0300

    Add exception logging through Sentry

--- kore/settings.py
+++ kore/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'raven.contrib.django.raven_compat',
     'rest_framework',
 
     'schools',
