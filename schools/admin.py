77d72fe0502c64294dbacdbf8defbb44ee21c088
Juha Yrjölä <juha.yrjola@iki.fi>
2015-04-24 19:40:22 +0300

    Add search based on school name

--- schools/admin.py
+++ schools/admin.py
@@ -10,6 +10,7 @@ class SchoolBuildingPhotoInline(admin.TabularInline):
 class SchoolBuildingAdmin(admin.ModelAdmin):
     fields = ('school', 'building', 'begin_year', 'end_year')
     readonly_fields = fields
+    search_fields = ['school__names__types__value']
     list_display = ('__str__', 'has_photo')
     list_filter = ('photos',)
     inlines = [SchoolBuildingPhotoInline]
